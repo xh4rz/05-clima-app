7071c707b200a2ccdf5f5230e3514aafc1821e40
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-03 16:45:57 -0500

    69. Menu de la aplicación

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const { leerInput } = require('./helpers/inquirer');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 
 const main = async () => {
-	const texto = await leerInput('Hola:');
+	let opt;
 
-	console.log(texto);
+	do {
+		opt = await inquirerMenu();
+
+		console.log({ opt });
+
+		if (opt !== 0) await pausa();
+	} while (opt !== 0);
 };
 
 main();
