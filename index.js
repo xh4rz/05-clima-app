02cdef37d24030bb943cef27995088014d37986c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-05-03 23:51:09 -0500

    70. Modelo para controlar la aplicación

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 const main = async () => {
+	const busquedas = new Busquedas();
+
 	let opt;
 
 	do {
 		opt = await inquirerMenu();
 
-		console.log({ opt });
+		switch (opt) {
+			case 1:
+				// Mostrar mensaje
+				const lugar = await leerInput('Ciudad: ');
+				console.log(lugar);
+
+				// Buscar los lugares
+
+				// Seleccionar el lugar
+
+				// Clima
+
+				// Mostrar resultados
+
+				console.log('\nInformación de la ciudad\n'.green);
+				console.log('Ciudad:');
+				console.log('Lat:');
+				console.log('Lng:');
+				console.log('Temperatura:');
+				console.log('Mínima:');
+				console.log('Máxima:');
+		}
 
 		if (opt !== 0) await pausa();
 	} while (opt !== 0);
---
+++ models/busquedas.js
@@ -0,0 +1,17 @@
+class Busquedas {
+	historial = ['Tegucigalpa', 'Madrid', 'San José'];
+
+	constructor() {
+		// TODO: leer DB si existe
+	}
+
+	async ciudad(lugar = '') {
+		// peticion http
+
+		console.log(lugar);
+
+		return []; // retornar los lugares
+	}
+}
+
+module.exports = Busquedas;
